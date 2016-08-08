23203fdb82a5ad73c8165ece871df13c1987275e
Rikuo Hasegawa <r-hasegawa@x.colopl.co.jp>
2016-08-08 04:40:08 -0400

    メモコメントを追加

--- polls/models.py
+++ polls/models.py
@@ -17,7 +17,7 @@ class Question(models.Model):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) #これでQuestion型のインスタンスからChoiceの参照がchoice_set_allなどから取得できるようになるらしい。typename_setって感じ？
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
