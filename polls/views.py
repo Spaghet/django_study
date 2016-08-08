d009781e7395497c1893cab7a94bc4bc4fed0a9b
Rikuo Hasegawa <r-hasegawa@x.colopl.co.jp>
2016-08-08 15:44:35 -0400

    generic viewでの実装

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,41 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
+
+# Generic Views
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list" #set the "context object"'s name explicitly instead of auto-generating the name  question_list
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
 # Create your views here.
+
+#deprecated
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = { "latest_question_list" : latest_question_list }
     return render(request, "polls/index.html", context)
 
-#404 error
+#deprecated
 def detail(request,question_id):
     question = get_object_or_404(Question, pk=question_id) #pk = primary key
     return render(request, "polls/detail.html", {"question": question})
 
+#deprecated
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/results.html", {"question": question})
